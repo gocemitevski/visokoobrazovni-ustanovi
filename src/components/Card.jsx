ca0c7a5dee3b3feef1d44541a9c099904514bfc7
Goce Mitevski <goce.mitevski@gmail.com>
2025-10-05 20:35:12 +0200

    Additionally mark counters with selected filters

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,11 @@ import { count } from "../utils/counter";
 
 export default function Card({ data, filters, index, title, titlePlural }) {
   return (
-    <div className={`card text-bg-light`}>
+    <div
+      className={`card ${
+        filters[Object.keys(filters)[index]] ? `border-success border-2` : ``
+      } bg-light`}
+    >
       <div className="card-body">
         <h5
           className={`card-title ${
