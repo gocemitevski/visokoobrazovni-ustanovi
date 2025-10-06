2f84536d43e612a10462b2f417505978c8db70ab
Goce Mitevski <goce.mitevski@gmail.com>
2025-10-06 13:59:30 +0200

    Add placeholders

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -284,9 +284,15 @@ export default function Table({ tableData }) {
         </div>
       ) : (
         <Fragment>
+          <div className="placeholder-glow mt-3">
+            <span className="placeholder col-12" style={{ height: 60 }}></span>
+          </div>
+          <div className="placeholder-glow mt-3">
+            <span className="placeholder col-12" style={{ height: 200 }}></span>
+          </div>
           {[...Array(8)].map((_, index) => (
             <p key={index} className="placeholder-glow my-2">
-              <span className="placeholder col-12"></span>
+              <span className="placeholder h-50 col-12"></span>
             </p>
           ))}
         </Fragment>
